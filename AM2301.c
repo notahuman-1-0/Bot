2b104409f16226fb035b5e5877d2382609d8f4ea
Srinivas T B <57246992+notahuman-1-0@users.noreply.github.com>
2023-04-08 13:24:03 +0530

    Add files via upload

---
+++ AM2301.c
@@ -0,0 +1,41 @@
+  /* 
+modified on Dec 29, 2020
+Modified by MehranMaleki from Arduino Examples
+Home
+*/
+
+
+//Libraries
+#include <DHT.h>;
+
+//Constants
+#define DHTPIN 2            //what pin we're connected to
+#define DHTTYPE DHT21       //DHT 21  (AM2301)
+DHT dht(DHTPIN, DHTTYPE);   //Initialize DHT sensor for normal 16mhz Arduino
+
+
+//Variables
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop()
+{
+  //Read data and store it to variables hum and temp
+  hum = dht.readHumidity();
+  temp= dht.readTemperature();
+  
+  //Print temp and humidity values to serial monitor
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print("%,  Temperature: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
+  
+  delay(2000); //Delay 2 sec.
+}
